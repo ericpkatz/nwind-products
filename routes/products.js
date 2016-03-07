da51b85b77ba2b85e17e38684a9a4f20cefbcbfd
Eric P. Katz <ericpkatz@gmail.com>
2016-03-06 20:27:05 -0500

    dry out routes

--- routes/products.js
+++ routes/products.js
@@ -3,112 +3,107 @@ var Product = require('../db').models.Product;
 
 module.exports = app;
 
-function update(id, body){
-  return Product.findById(id)
-    .then(function(product){
-      if(body.discontinued !== undefined)
-        product.discontinued = body.discontinued;
-      if(body.numberInStock !== undefined)
-        product.numberInStock = body.numberInStock;
-      return product.save();
-    });
-}
 
 function getSelected(products, name){
-  return products.filter(function(product){
-    return product.name === name;
-  })[0];
 }
 
 app.use(function(req, res, next){
+  res.locals.getDeleteAction = function(title, product){
+    if(title === 'Products')
+      return `/products/${product.id}`;
+    return `/products/active/${product.id}`;
+  };
+
   res.locals.getEditAction = function(title, product){
     if(title === 'Products')
       return `/products/${product.id}`;
     return `/products/active/${product.id}`;
   };
-  next();
-});
 
-app.use(function(req, res, next){
   res.locals.getInsertAction = function(title){
     if(title === 'Products')
       return `/products/`;
     return `/products/active`;
   };
+
   next();
-
-});
-
-app.post('/', function(req, res, next){
-  Product.create(req.body)
-    .then(function(product){
-      res.redirect('/products');
-    }, next);
-});
-
-app.delete('/:id', function(req, res, next){
-  Product.remove({ _id: req.params.id })
-    .then(function(product){
-      res.redirect('/products');
-    }, next);
-});
-
-app.delete('/active/:id', function(req, res, next){
-  Product.remove({ _id: req.params.id })
-    .then(function(product){
-      res.redirect('/products/active');
-    }, next);
-});
-
-app.put('/:id', function(req, res, next){
-  update(req.params.id, req.body)
-    .then(function(product){
-      res.redirect('/products');
-    }, next);
-});
-
-app.put('/active/:id', function(req, res, next){
-  update(req.params.id, req.body)
-    .then(function(product){
-      res.redirect('/products/active');
-    }, next);
-});
-
-app.post('/active', function(req, res, next){
-  Product.create(req.body)
-    .then(function(product){
-      res.redirect('/products/active');
-    }, next);
-});
-
-app.get('/', function(req, res, next){
-  Product.find()
-    .then(function(products){
-      res.render('products', { title: 'Products', mode: 'products', products: products });
-    });
 });
 
 
-app.get('/active', function(req, res, next){
-  Product.find({ discontinued: false })
-    .then(function(products){
-      res.render('products', { title: 'Active Products', mode: 'products', products: products });
-    });
-});
+function deleteRoute(redirectTo){
+  return function(req, res, next){
+    Product.remove({ _id: req.params.id })
+      .then(function(product){
+        res.redirect(redirectTo);
+      }, next);
+  };
+}
+
+function updateRoute(redirectTo){
+  return function(req, res, next){
+    Product.findById(req.params.id)
+      .then(function(product){
+        if(req.body.discontinued !== undefined)
+          product.discontinued = req.body.discontinued;
+        if(req.body.numberInStock !== undefined)
+          product.numberInStock = req.body.numberInStock;
+        return product.save();
+      })
+      .then(function(){
+        res.redirect(redirectTo);
+      });
+  };
+}
+
+function createRoute(redirectTo){
+  return function(req, res, next){
+    Product.create(req.body)
+      .then(function(product){
+        res.redirect(redirectTo);
+      }, next);
+  };
+
+}
+
+function listRoute(filter, title){
+  return function(req, res, next){
+    Product.find(filter)
+      .then(function(products){
+        res.render('products', { title: title, mode: 'products', products: products });
+      }, next);
+      
+  };
+
+}
+
+function detailRoute(filter, title){
+  return function(req, res, next){
+    Product.find(filter)
+      .then(function(products){
+        var selected = products.filter(function(product){
+          return product.name = req.params.name;
+        })[0]; 
+        res.render('products', { title: title, mode: 'products', products: products, selected: selected });
+      });
+  }
+}
+app.delete('/:id', deleteRoute('/products')); 
+
+app.delete('/active/:id', deleteRoute('/products/active'));
+
+app.put('/:id', updateRoute('/products')); 
+
+app.put('/active/:id', updateRoute('/products/active')); 
+
+app.post('/', createRoute('/products')); 
+
+app.post('/active', createRoute('/products/active'));
+
+app.get('/', listRoute({}, 'Products'));
+
+app.get('/active', listRoute({ discontinued: false }, 'Active Products'));
 
 
-app.get('/active/:name', function(req, res, next){
-  Product.find({ discontinued: false })
-    .then(function(products){
-      var selected = getSelected(products, req.params.name);
-      res.render('products', { title: 'Active Products', mode: 'products', products: products, selected: selected });
-    });
-});
+app.get('/active/:name', detailRoute({ discontinued: false }, 'Active Products')); 
 
-app.get('/:name', function(req, res, next){
-  Product.find()
-    .then(function(products){
-      var selected = getSelected(products, req.params.name);
-      res.render('products', { title: 'Products', mode: 'products', products: products, selected: selected });
-    });
-});
+app.get('/:name', detailRoute({}, 'Products')); 
